993dd0df4c3ff66ae6d829dcdf593071e3c8cac5
Victor Verma-Bonany <victor@Victors-MacBook-Air.local>
2026-05-14 14:36:39 -0400

    Make EV Scanner installable app

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ev-scanner-v1';
+const APP_SHELL = [
+  './',
+  './ev-scanner.html',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin) return;
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./ev-scanner.html') || caches.match('./index.html')))
+  );
+});
